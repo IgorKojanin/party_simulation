47ac1f5707cfd1dbecb092c84449066af668dff9
unknown <wpv825@haw-hamburg.de>
2023-10-20 09:17:49 +0200

    Avatar-FirstCommit

---
+++ src/com/simulation/avatar/Avatar.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.simulation.avatar;
+
+/**
+ * 
+ */
+public class Avatar {
+	private String color;
+	private int borderWidth;
+	private int avatarId;
+	private int avatarAge;
+	String[] Hobbies = new String[3];
+	
+}
