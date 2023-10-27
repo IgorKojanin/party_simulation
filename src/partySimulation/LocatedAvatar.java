3cc836f6c47b49c385ba06c4bec4faaf1724e769
EliyasT <eliyasbirhanu1991@gmail.com>
2023-10-27 19:33:22 +0200

    update Matrix and LocatedAvatar

--- src/partySimulation/LocatedAvatar.java
+++ src/partySimulation/LocatedAvatar.java
@@ -7,36 +7,56 @@ public class LocatedAvatar {
 	private int x;
 	private int y;
 	private Heading heading;
-	
-	public LocatedAvatar(Avatar avatar,int x,int y,Heading heading) {
+
+	public LocatedAvatar(Avatar avatar, int x, int y) {
 		this.avatar = avatar;
 		this.x = x;
 		this.y = y;
 		this.heading = Heading.WEST;
 	}
-	
+
 	public void setX(int x) {
 		this.x = x;
 	}
-	
+
 	public void setY(int y) {
 		this.y = y;
 	}
-	
+
 	public int getX() {
 		return x;
 	}
-	
+
 	public int getY() {
 		return y;
 	}
-	
+
 	public Avatar getAvatar() {
 		return avatar;
 	}
 
 	public Heading getHeading() {
-		
 		return heading;
+	}
+
+	public void incX() {
+		x++;
+	}
+
+	public void decX() {
+		x--;
+	}
+
+	public void incY() {
+		y++;
+	}
+
+	public void decY() {
+
+	}
+
+	public void setHeading(Heading heading) {
+		this.heading = heading;
+
 	}
 }
--- src/partySimulation/Matrix.java
+++ src/partySimulation/Matrix.java
@@ -3,41 +3,106 @@ package partySimulation;
 import java.util.ArrayList;
 
 import com.simulation.avatar.Avatar;
+import com.simulation.avatar.Bouncer;
+import com.simulation.enums.Color;
 import com.simulation.enums.Direction;
+import com.simulation.enums.Shape;
+import com.simulation.enviroment.MyFrame;
 
 public class Matrix {
 
 	private MyFrame env;
 	private ArrayList<LocatedAvatar> avatars;
-	
+
 	public Matrix() {
 		env = new MyFrame();
 		avatars = new ArrayList<LocatedAvatar>();
+		Bouncer bouncer = new Bouncer(Shape.CIRCLE, Color.RED, 0, 0, 0, 0, null, null);
+		LocatedAvatar locBouuncer = new LocatedAvatar(bouncer, 0, 0);
+		avatars.add(locBouuncer);
+
 	}
 
 	public void run() {
-		while(true) {
-			for(LocatedAvatar locAvatar : avatars) {
-				
+		while (true) {
+			for (LocatedAvatar locAvatar : avatars) {
+
+				int oldX = locAvatar.getX();
+				int oldY = locAvatar.getY();
 				Direction dir = locAvatar.getAvatar().moveAvatar();
-				switch(dir) {
-					case Direction.FORWARD:
-						if(locAvatar.getHeading()==Heading.WEST) {
-							if(env.isUsable(locAvatar.getX()+1,locAvatar.getY()))
-							locAvatar.setX(locAvatar.getX()+1);
-						}else if(locAvatar.getHeading()==Heading.EAST) {
-							if(env.isUsable(locAvatar.getX()-1,locAvatar.getY()))
-								locAvatar.setX(locAvatar.getX()-1);
-					}else if(locAvatar.getHeading()==Heading.NORTH) {
-						if(env.isUsable(locAvatar.getX(),locAvatar.getY()-1))
-							locAvatar.setX(locAvatar.getY()-1);
-					}else if(locAvatar.getHeading()==Heading.SOUTH) {
-						if(env.isUsable(locAvatar.getX(),locAvatar.getY()+1))
-							locAvatar.setX(locAvatar.getY()+1);
+				switch (dir) {
+				case FORWARD:
+					if (locAvatar.getHeading() == Heading.WEST) {
+						if (env.isUsable(locAvatar.getX() + 1, locAvatar.getY()))
+							locAvatar.incX();
+					} else if (locAvatar.getHeading() == Heading.EAST) {
+						if (env.isUsable(locAvatar.getX() - 1, locAvatar.getY()))
+							locAvatar.decX();
+					} else if (locAvatar.getHeading() == Heading.NORTH) {
+						if (env.isUsable(locAvatar.getX(), locAvatar.getY() - 1))
+							locAvatar.decY();
+					} else if (locAvatar.getHeading() == Heading.SOUTH) {
+						if (env.isUsable(locAvatar.getX(), locAvatar.getY() + 1))
+							locAvatar.incY();
+					}
+				case BACK:
+					if (locAvatar.getHeading() == Heading.WEST) {
+						if (env.isUsable(locAvatar.getX() - 1, locAvatar.getY()))
+							locAvatar.decX();
+						locAvatar.setHeading(Heading.EAST);
+					} else if (locAvatar.getHeading() == Heading.EAST) {
+						if (env.isUsable(locAvatar.getX() + 1, locAvatar.getY()))
+							locAvatar.incX();
+						locAvatar.setHeading(Heading.WEST);
+					} else if (locAvatar.getHeading() == Heading.NORTH) {
+						if (env.isUsable(locAvatar.getX(), locAvatar.getY() + 1))
+							locAvatar.incY();
+						locAvatar.setHeading(Heading.SOUTH);
+					} else if (locAvatar.getHeading() == Heading.SOUTH) {
+						if (env.isUsable(locAvatar.getX(), locAvatar.getY() - 1))
+							locAvatar.decY();
+						locAvatar.setHeading(Heading.NORTH);
+					}
+				case RIGHT:
+					if (locAvatar.getHeading() == Heading.WEST) {
+						if (env.isUsable(locAvatar.getX(), locAvatar.getY() - 1))
+							locAvatar.decY();
+						locAvatar.setHeading(Heading.NORTH);
+					} else if (locAvatar.getHeading() == Heading.EAST) {
+						if (env.isUsable(locAvatar.getX(), locAvatar.getY() + 1))
+							locAvatar.incY();
+						locAvatar.setHeading(Heading.SOUTH);
+					} else if (locAvatar.getHeading() == Heading.NORTH) {
+						if (env.isUsable(locAvatar.getX() - 1, locAvatar.getY()))
+							locAvatar.decX();
+						locAvatar.setHeading(Heading.EAST);
+					} else if (locAvatar.getHeading() == Heading.SOUTH) {
+						if (env.isUsable(locAvatar.getX() + 1, locAvatar.getY()))
+							locAvatar.incX();
+						locAvatar.setHeading(Heading.WEST);
+					}
+				case LEFT:
+					if (locAvatar.getHeading() == Heading.WEST) {
+						if (env.isUsable(locAvatar.getX(), locAvatar.getY() + 1))
+							locAvatar.incY();
+						locAvatar.setHeading(Heading.SOUTH);
+					} else if (locAvatar.getHeading() == Heading.EAST) {
+						if (env.isUsable(locAvatar.getX(), locAvatar.getY() - 1))
+							locAvatar.decY();
+						locAvatar.setHeading(Heading.NORTH);
+					} else if (locAvatar.getHeading() == Heading.NORTH) {
+						if (env.isUsable(locAvatar.getX() + 1, locAvatar.getY()))
+							locAvatar.incX();
+						locAvatar.setHeading(Heading.WEST);
+					} else if (locAvatar.getHeading() == Heading.SOUTH) {
+						if (env.isUsable(locAvatar.getX() - 1, locAvatar.getY()))
+							locAvatar.decX();
+						locAvatar.setHeading(Heading.EAST);
 					}
 				}
+				env.moveTo(oldX, oldY, locAvatar.getX(), locAvatar.getY());
 			}
 		}
-		
+
 	}
 }
