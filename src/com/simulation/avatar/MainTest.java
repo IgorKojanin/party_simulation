635215a78fd3fa0605c2f244b59f6b97338f8812
Bernhard_Ruediger <bernhard.ruediger@haw-hamburg.de>
2023-10-27 09:41:43 +0200

    Committing to push environment changes into main

--- src/com/simulation/avatar/MainTest.java
+++ src/com/simulation/avatar/MainTest.java
@@ -1,9 +1,10 @@
 package com.simulation.avatar;
+import com.simulation.enviroment.MyFrame;
 
 public class MainTest {
 
 	public static void main(String[] args) {
 		Bouncer name = new Bouncer(null, null, 0, 0, 0, 0, null, null); 
-
+		new MyFrame();
 	} 
 }
