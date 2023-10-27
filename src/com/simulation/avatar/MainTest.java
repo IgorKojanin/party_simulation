8d0aa8b80afb53c8498b69d0a505bf7aa578b39f
Thursday2008 <alisa.khrust@gmail.com>
2023-10-27 18:26:45 +0200

    Moved MyPanel inside MyFrame class (called GridPanel now), implemented test 'movement' of a blue square on the plan using the moveObject method in main()

--- src/com/simulation/avatar/MainTest.java
+++ src/com/simulation/avatar/MainTest.java
@@ -5,6 +5,14 @@ import com.simulation.enums.Color;
 import com.simulation.enums.Shape;
 
 public class MainTest {
+	public static void wait(int ms) {
+		try {
+			Thread.sleep(ms);
+		} catch (InterruptedException ex) {
+			Thread.currentThread().interrupt();
+
+		}
+	}
 
 	public static void main(String[] args) {
 		PartyGoer avatarJoe = new PartyGoer(Shape.CIRCLE, Color.RED, 1, 2, 3, 4, null, null); 
@@ -17,9 +25,26 @@ public class MainTest {
 		System.out.println("My Place priorities: " + avatarJoe.getPlacePriorities());
 		System.out.println("My questions and answers: " + avatarJoe.getQuestionsAnswersList());
 		System.out.println("My randomely chosen direction: " + avatarJoe.moveAvatar());
-		
-		Bartender bartenderSue = new Bartender(Shape.SQUARE, Color.BLUE, 10, 11, 12, 13, null, null); 
+
+		Bartender bartenderSue = new Bartender(Shape.SQUARE, Color.BLUE, 10, 11, 12, 13, null, null);
 		System.out.println("Color: " + bartenderSue.getColor());
-		new MyFrame();
-	} 
+
+		// DRAWING THE FLOORPLAN
+		MyFrame frame = new MyFrame();
+		frame.setVisible(true);
+
+		// EXAMPLE MOVING AVATAR BY CHANGING SQUARE COLOR
+		wait(1000);
+		frame.moveObject(7,34, 6, 34);
+		wait(1000);
+		frame.moveObject(6,34, 5, 34);
+		wait(1000);
+		frame.moveObject(5,34, 5, 33);
+		wait(1000);
+		frame.moveObject(5,33, 5, 32);
+		wait(1000);
+		frame.moveObject(5,32, 5, 31);
+		wait(1000);
+		frame.moveObject(5,31, 5, 30);
+	}
 }
--- src/com/simulation/enviroment/Square.java
+++ src/com/simulation/enviroment/Square.java
@@ -8,15 +8,11 @@ public class Square extends java.awt.Rectangle {
 	private Color color;
 	private Places place;
 
- 
-
     public Square(int x, int y, int width, int height, Color color, Places place) {
         super(x, y, width, height);
         this.color = color;
         this.place = place;
     }
-
- 
 
     public Color getColor() {
         return color;
