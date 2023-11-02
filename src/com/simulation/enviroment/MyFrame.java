1ab1e8c1f7f6923a7e39f9cdd4818ee9bd0ef5c6
Mynul Islam <mynulislam042@gmail.com>
2023-11-03 00:00:03 +0100

    Avatar label with name, age, id

--- src/com/simulation/enviroment/MyFrame.java
+++ src/com/simulation/enviroment/MyFrame.java
@@ -2,6 +2,11 @@ package com.simulation.enviroment;
 
 import javax.swing.*;
 
+import com.simulation.avatar.Avatar;
+
+import java.awt.Color;
+import java.awt.Dimension;
+
 public class MyFrame extends JFrame {
 	MyPanel panel;
 
@@ -13,4 +18,45 @@ public class MyFrame extends JFrame {
 		this.setLocationRelativeTo(null);
 		this.setVisible(true);
 	}
+	
+
+
+	public class AvatarLabel extends JLabel {
+	    
+	    private Avatar avatar;
+
+	    public AvatarLabel(Avatar avatar) {
+	        this.avatar = avatar;
+	        
+	        // Initialize the label with avatar properties
+	        this.setText("Avatar " + avatar.getId()); 
+	        this.setOpaque(true);
+	        this.setBackground(convertColor(avatar.getColor()));
+	        this.setPreferredSize(new Dimension(50, 50)); /
+	        this.setToolTipText("Avatar Name: " + avatar.getShape().toString());
+	        
+	        
+	        if (avatar.getBorderWidth() > 0) {
+	            setBorder(BorderFactory.createLineBorder(java.awt.Color.BLACK, avatar.getBorderWidth()));
+	        }
+	    }
+	    
+
+	    private Color convertColor(com.simulation.enums.Color colorEnum) {
+	        switch (colorEnum) {
+	            case RED:
+	                return java.awt.Color.RED;
+	            case GREEN:
+	                return java.awt.Color.GREEN;
+	            case BLUE:
+	                return java.awt.Color.BLUE;
+	           
+	            default:
+	                return java.awt.Color.BLACK; // Default color
+	        }
+	    }
+	    
+	    
+	}
+
 }
