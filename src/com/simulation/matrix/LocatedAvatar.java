9b6f921983457bbf125fb8221cd681cdf32a7039
unknown <wfu634@haw-hamburg.de>
2023-11-03 11:11:40 +0100

    refactoring

---
+++ src/com/simulation/matrix/LocatedAvatar.java
@@ -0,0 +1,66 @@
+package com.simulation.matrix;
+
+import com.simulation.avatar.Avatar;
+
+public class LocatedAvatar {
+	private Avatar avatar;
+	private int x;
+	private int y;
+	private Heading heading;
+
+	public LocatedAvatar(Avatar avatar, int x, int y) {
+		this.avatar = avatar;
+		this.x = x;
+		this.y = y;
+		this.heading = Heading.WEST;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public Avatar getAvatar() {
+		return avatar;
+	}
+
+	public Heading getHeading() {
+		return heading;
+	}
+
+	public void incX() {
+		x++;
+	}
+
+	public void decX() {
+		x--;
+	}
+
+	public void incY() {
+		y++;
+	}
+
+	public void decY() {
+		y--;
+	}
+
+	public void setHeading(Heading heading) {
+		this.heading = heading;
+
+	}
+
+	public Object getColor() {
+		return avatar.getColor();
+	}
+}
---
+++ src/com/simulation/matrix/Matrix.java
@@ -0,0 +1,96 @@
+package com.simulation.matrix;
+
+import java.util.ArrayList;
+
+import com.simulation.avatar.Avatar;
+import com.simulation.avatar.Bouncer;
+import com.simulation.enums.Color;
+import com.simulation.enums.Direction;
+import com.simulation.enums.Shape;
+import com.simulation.enviroment.MyFrame;
+
+public class Matrix {
+
+	private MyFrame env;
+	private ArrayList<LocatedAvatar> avatars;
+
+	public Matrix() {
+		env = new MyFrame();
+		avatars = new ArrayList<LocatedAvatar>();
+		Bouncer bouncer = new Bouncer(Shape.CIRCLE, Color.RED, 0, 0);
+		LocatedAvatar locBouncer = new LocatedAvatar(bouncer, 0, 0);
+		avatars.add(locBouncer);
+
+	}
+
+	public void run() {
+		while (true) {
+			for (LocatedAvatar locAvatar : avatars) {
+
+				int oldX = locAvatar.getX();
+				int oldY = locAvatar.getY();
+				Direction dir = locAvatar.getAvatar().moveAvatar();
+				switch (dir) {
+				case FORWARD:
+					switch(locAvatar.getHeading()) {
+					case WEST:changeXY(locAvatar, ChangeInXY.DECX);
+					case EAST: changeXY(locAvatar, ChangeInXY.INCX);
+					case NORTH:changeXY(locAvatar, ChangeInXY.DECY);
+					case SOUTH:changeXY(locAvatar, ChangeInXY.INCY);			
+					}
+				case BACK:
+					switch(locAvatar.getHeading()) {
+					case WEST:changeXY(locAvatar, ChangeInXY.INCX);
+					case EAST: changeXY(locAvatar, ChangeInXY.DECX);
+					case NORTH:changeXY(locAvatar, ChangeInXY.INCY);
+					case SOUTH:changeXY(locAvatar, ChangeInXY.DECY);			
+					}
+				case RIGHT:
+					switch(locAvatar.getHeading()) {
+					case WEST:changeXY(locAvatar, ChangeInXY.DECY);
+					case EAST: changeXY(locAvatar, ChangeInXY.INCY);
+					case NORTH:changeXY(locAvatar, ChangeInXY.INCX);
+					case SOUTH:changeXY(locAvatar, ChangeInXY.DECX);			
+					}
+				case LEFT:
+					switch(locAvatar.getHeading()) {
+					case WEST:changeXY(locAvatar, ChangeInXY.INCY);
+					case EAST: changeXY(locAvatar, ChangeInXY.DECY);
+					case NORTH:changeXY(locAvatar, ChangeInXY.DECX);
+					case SOUTH:changeXY(locAvatar, ChangeInXY.INCX);			
+					}
+				}
+				env.moveTo(oldX, oldY, locAvatar.getX(), locAvatar.getY(), locAvatar.getColor());
+			}
+		}
+	}
+
+	private void changeXY(LocatedAvatar locAvatar, ChangeInXY c) {
+		switch (c) {
+		case INCX:
+			if (env.isUsable(locAvatar.getX() + 1, locAvatar.getY())) {
+				locAvatar.incX();
+				locAvatar.setHeading(Heading.EAST);
+			}
+
+		case DECX:
+			if (env.isUsable(locAvatar.getX() - 1, locAvatar.getY())) {
+				locAvatar.decX();
+				locAvatar.setHeading(Heading.WEST);
+			}
+
+		case INCY:
+			if (env.isUsable(locAvatar.getX(), locAvatar.getY() + 1)) {
+				locAvatar.incY();
+				locAvatar.setHeading(Heading.SOUTH);
+			}
+
+		case DECY:
+			if (env.isUsable(locAvatar.getX(), locAvatar.getY() - 1)) {
+				locAvatar.decY();
+				locAvatar.setHeading(Heading.NORTH);
+			}
+
+		}
+	}
+}
