0b23a655c3fb33237e67d1168dd711b73d6a04f2
unknown <wpv825@haw-hamburg.de>
2023-11-03 10:37:31 +0100

    Add interface and rethinking of the avatar partygoer

---
+++ src/com/sim/partypeople/JoeMama.java
@@ -0,0 +1,101 @@
+///////////////////////////////////////////////////////////////////////////////
+//                   Party Simulator
+// Date:         27/10/2023
+//
+// Class: Joe.java
+// Description: Template for the people
+//
+///////////////////////////////////////////////////////////////////////////////
+package com.sim.partypeople;
+
+import com.simulation.avatar.Avatar;
+import com.simulation.avatar.PartyGoer;
+import com.simulation.enums.Color;
+import com.simulation.enums.Shape;
+
+public class JoeMama extends Avatar implements PartyGoer {
+	
+	// store locally where u are
+	// check with that exactly what can u do?
+	
+
+	// ************** Constructor **************
+	public JoeMama(Shape shape, Color color, int borderWidth, int avatarId, int avatarAge, int drinksConsumed) {
+		super(shape, color, borderWidth, avatarId, avatarAge, drinksConsumed);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void startDancing() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public void stopDancing() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public void setTimeout() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	public void drink() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public void fight() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public void talk() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public void smoke() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public void toilet() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public void playPool() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public void playFußt() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public void getWhatISee() {
+		// get function from simulation, returns array of Places enums. 2 places ahead
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public void asdfg() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
