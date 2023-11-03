efce52f3e8ab283d258dcaed7540f7ddace03ccb
Kieran Edgeworth <edgeworthkieran@gmail.com>
2023-11-03 10:36:41 +0100

    Changed moveTo method in MyFrame

--- src/com/simulation/enviroment/MyFrame.java
+++ src/com/simulation/enviroment/MyFrame.java
@@ -4,9 +4,6 @@ import com.simulation.enums.Places;
 
 import javax.swing.*;
 import java.awt.*;
-
-import com.simulation.avatar.Avatar;
-
 import java.awt.Color;
 import java.awt.Dimension;
 
@@ -95,17 +92,6 @@ public class MyFrame extends JFrame {
 		}
 	}
 
-	/* Original moveTo method
-	public void moveTo(int fromRow, int fromCol, int toRow, int toCol) {
-		if (squares[fromRow][fromCol].getColor() == Color.BLUE) {
-			squares[fromRow][fromCol].setColor(Color.WHITE); // Clear the from-square
-			repaint();
-			squares[toRow][toCol].setColor(Color.BLUE); // Set the to-square to blue
-			repaint();
-		}
-	}
-	*/
-
 	public void moveTo(int fromRow, int fromCol, int toRow, int toCol, Color color) {
         squares[fromRow][fromCol].setColor(squares[fromRow][fromCol].getBaseColor()); // Clear the from-square
 		squares[fromRow][fromCol].setIsUsable(true);
@@ -160,45 +146,4 @@ public class MyFrame extends JFrame {
 			g2d.drawLine(squareSize * 34, squareSize * 8, squareSize * 34, panelHeight - squareSize);
 		}
 	}
-	
-
-
-	public class AvatarLabel extends JLabel {
-	    
-	    private Avatar avatar;
-
-	    public AvatarLabel(Avatar avatar) {
-	        this.avatar = avatar;
-	        
-	        // Initialize the label with avatar properties
-	        this.setText("Avatar " + avatar.getId()); 
-	        this.setOpaque(true);
-	        this.setBackground(convertColor(avatar.getColor()));
-	        this.setPreferredSize(new Dimension(50, 50)); 
-	        this.setToolTipText("Avatar Name: " + avatar.getShape().toString());
-	        
-	        
-	        if (avatar.getBorderWidth() > 0) {
-	            setBorder(BorderFactory.createLineBorder(java.awt.Color.BLACK, avatar.getBorderWidth()));
-	        }
-	    }
-	    
-
-	    private Color convertColor(com.simulation.enums.Color colorEnum) {
-	        switch (colorEnum) {
-	            case RED:
-	                return java.awt.Color.RED;
-	            case GREEN:
-	                return java.awt.Color.GREEN;
-	            case BLUE:
-	                return java.awt.Color.BLUE;
-	           
-	            default:
-	                return java.awt.Color.BLACK; // Default color
-	        }
-	    }
-	    
-	    
-	}
-
 }
