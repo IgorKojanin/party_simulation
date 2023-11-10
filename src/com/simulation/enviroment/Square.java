6bf0bb3b6a9164ee6d3371013d94a9573a93596d
Bernhard_Ruediger <bernhard.ruediger@haw-hamburg.de>
2023-11-10 11:29:08 +0100

    Added ability to display text inside squares

--- src/com/simulation/enviroment/Square.java
+++ src/com/simulation/enviroment/Square.java
@@ -10,15 +10,17 @@ public class Square extends java.awt.Rectangle {
     private final Color baseColor;
 	private Boolean isUsable;
 	private final Places place;
+    private String displayletter;
 
  
 
-    public Square(int x, int y, int width, int height, Color baseColor, boolean isUsable, Places place) {
+    public Square(int x, int y, int width, int height, Color baseColor, boolean isUsable, Places place, String displayletter) {
         super(x, y, width, height);
         this.color = baseColor;
         this.place = place;
         this.isUsable = isUsable;
         this.baseColor = baseColor;
+        this.displayletter = displayletter;
     }
 
     public Color getColor() {
@@ -46,9 +48,15 @@ public class Square extends java.awt.Rectangle {
     	return isUsable;
     }
 
-    public void setIsUsable(boolean isUsable) { 
+    public String getDisplayLetter() {
+        return this.displayletter;
+    }
+
+    public void setIsUsable(boolean isUsable) {
         this.isUsable = isUsable;
     }
 
-
+    public void setDisplayLetter(String displayletter) {
+        this.displayletter = displayletter;
+    }
 }
