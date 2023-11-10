25a367d3be528782121143af1ed278bf5e998b10
Igor <igorkojanin@gmail.com>
2023-11-10 10:46:43 +0100

    Added more drinks for bartender

--- src/com/simulation/avatar/Bartender.java
+++ src/com/simulation/avatar/Bartender.java
@@ -20,6 +20,8 @@ private static final int LEGAL_STRONG_ALCOHOL_AGE = 18;
 private static final int LEGAL_WEAK_ALCOHOL_AGE = 16;
 private static final int ADD_BEER_PERCENTAGE = 10;
 private static final int ADD_VODKA_PERCENTAGE = 40;
+private static final int ADD_MOJITO_PERCENTAGE = 20;
+private static final int ADD_RUM_AND_COKE_PERCENTAGE = 25;
 
 	public int checkAge(Avatar avatar) {
 		return avatar.getAge();
@@ -28,15 +30,29 @@ private static final int ADD_VODKA_PERCENTAGE = 40;
 	private void serveDrink(Avatar avatar, BeverageType type) {
 		switch (type) {
 		case BEER:
-			if (checkAge(avatar) > LEGAL_WEAK_ALCOHOL_AGE) { // avatars can drink beer if they are above 16 years old
+			if (checkAge(avatar) > LEGAL_WEAK_ALCOHOL_AGE) { // Avatars can drink beer if they are above 16 years old
 				avatar.setAlcoholPercentage(avatar.getAlcoholPercentage()+ADD_BEER_PERCENTAGE);
 			}
 			else
 				System.out.println("Bartender: Sorry your'e too young");
 			break;
 		case VODKA:
-			if (checkAge(avatar) > LEGAL_STRONG_ALCOHOL_AGE) { // avatars can drink vodka if they are above 18 years old
+			if (checkAge(avatar) > LEGAL_STRONG_ALCOHOL_AGE) { // Avatars can drink Vodka if they are above 18 years old
 				avatar.setAlcoholPercentage(avatar.getAlcoholPercentage()+ADD_VODKA_PERCENTAGE);
+			}
+			else
+				System.out.println("Bartender: Sorry your'e too young");
+			break;
+		case MOJITO:
+			if (checkAge(avatar) > LEGAL_STRONG_ALCOHOL_AGE) {
+				avatar.setAlcoholPercentage(avatar.getAlcoholPercentage()+ADD_MOJITO_PERCENTAGE);
+			}
+			else
+				System.out.println("Bartender: Sorry your'e too young");
+			break;
+		case RUM_AND_COKE:
+			if (checkAge(avatar) > LEGAL_STRONG_ALCOHOL_AGE) { 
+				avatar.setAlcoholPercentage(avatar.getAlcoholPercentage()+ADD_RUM_AND_COKE_PERCENTAGE);
 			}
 			else
 				System.out.println("Bartender: Sorry your'e too young");
@@ -44,7 +60,6 @@ private static final int ADD_VODKA_PERCENTAGE = 40;
 		case WATER: // drinking water lowers the Alcohol percentage 
 			avatar.setAlcoholPercentage(avatar.getAlcoholPercentage()-10);
 			break;
-		
 		}
 	}
 	
--- src/com/simulation/enums/BeverageType.java
+++ src/com/simulation/enums/BeverageType.java
@@ -3,5 +3,7 @@ package com.simulation.enums;
 public enum BeverageType {
     BEER,
     VODKA,
+    MOJITO,
+    RUM_AND_COKE,
     WATER
 }
