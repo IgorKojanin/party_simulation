a8ea395dab5f95286d67d79d32788f5030055038
unknown <wot792@haw-hamburg.de>
2023-12-08 11:12:09 +0100

    added primitive movement pattern

--- src/com/simulation/partypeople/Anatoly.java
+++ src/com/simulation/partypeople/Anatoly.java
@@ -9,11 +9,13 @@
 package com.simulation.partypeople;
 
 import java.awt.Color;
+import java.util.HashMap;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 import com.simulation.avatar.Avatar;
 import com.simulation.enums.Direction;
-import com.simulation.enums.Shape;
+import com.simulation.enums.*;
 
 public class Anatoly extends Avatar{
 
@@ -70,6 +72,26 @@ public class Anatoly extends Avatar{
 	}
 
 	public Direction moveAvatar() {
+ 
+		HashMap<Places, Integer> placesPriorities = new HashMap<Places, Integer>();
+		placesPriorities.put(Places.BAR, 1);
+		placesPriorities.put(Places.BOUNCER, 2);
+		placesPriorities.put(Places.DANCEFLOOR, 3);
+		placesPriorities.put(Places.DJ, 4);
+		
+		System.out.println(super.getWhatISee()[0]);
+		
+		Places myCurrentPlace = super.getWhatISee()[0];
+
+		
+		// have an empty array of squares 
+		// whenever the avatar moved, add the square and its type to the list 
+		// if the list is complete start moving based on the priority hashmap
+		
+		// move randomly until you find a place, remain idle for a determined/random ammount of time
+		
+		System.out.println(placesPriorities);
+		
 		Random rand = new Random();
 		int number = rand.nextInt(4);
 
@@ -83,7 +105,27 @@ public class Anatoly extends Avatar{
 		} else if (number == 3) {
 			dir = Direction.LEFT;
 		}
+		
+		
+		if(myCurrentPlace == Places.FUSSBALL) {
+			try {
+				TimeUnit.SECONDS.sleep(5);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		} else if(myCurrentPlace == Places.DANCEFLOOR) {
+			try {
+				TimeUnit.SECONDS.sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
 		return dir;
+		
+		
+	
 
         // define a variable that would hold a list of priorities: sort places on a scale of 1-5
         // check the map of places discovered, if it's empty randomly head to places until everything is discovered and stored
